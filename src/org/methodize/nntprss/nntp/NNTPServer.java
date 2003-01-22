99edff024e5f3e6d09ebcd6a911a55d8cb2ed54b
jasonbrome <jasonbrome>
2003-01-22 05:07:16 +0000

    Added content type and configurable listener port

--- src/org/methodize/nntprss/nntp/NNTPServer.java
+++ src/org/methodize/nntprss/nntp/NNTPServer.java
@@ -2,7 +2,7 @@ package org.methodize.nntprss.nntp;
 
 /* -----------------------------------------------------------
  * nntp//rss - a bridge between the RSS world and NNTP clients
- * Copyright (c) 2002 Jason Brome.  All Rights Reserved.
+ * Copyright (c) 2002, 2003 Jason Brome.  All Rights Reserved.
  *
  * email: nntprss@methodize.org
  * mail:  Methodize Solutions
@@ -32,34 +32,55 @@ package org.methodize.nntprss.nntp;
 
 import java.net.Socket;
 
+import org.apache.log4j.Logger;
+import org.methodize.nntprss.rss.db.ChannelManagerDAO;
+import org.methodize.nntprss.util.AppConstants;
 import org.methodize.nntprss.util.SimpleThreadPool;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * @author Jason Brome <jason@methodize.org>
- * @version 0.1
+ * @version $Id: NNTPServer.java,v 1.2 2003/01/22 05:07:16 jasonbrome Exp $
  */
 
 public class NNTPServer {
 
+	private Logger log = Logger.getLogger(NNTPServer.class);
+
 	private NNTPServerListener listener = null;
 	private SimpleThreadPool simpleThreadPool;
+	private int listenerPort;
 
-	private static final int MAX_NNTP_CLIENT_THREADS = 20;
+	private static final int MAX_NNTP_CLIENT_THREADS = 5;
+
+	private int contentType = AppConstants.CONTENT_TYPE_MIXED;
+	private ChannelManagerDAO channelManagerDAO;
 
 	public NNTPServer() throws Exception {
 		simpleThreadPool =
 			new SimpleThreadPool("NNTP Client Handlers", "NNTP Client Thread", 20);
-		listener = new NNTPServerListener(this);
+		channelManagerDAO = ChannelManagerDAO.getChannelManagerDAO();
 	}
 
 	public void configure(Document config) {
-		// TODO configure NNTP port...
-		// TODO configure Maximum concurrent threads etc
+// TODO configure Maximum concurrent threads etc
+		Element rootElm = config.getDocumentElement();
+		Element adminConfig = (Element)rootElm.getElementsByTagName("nntp").item(0);
+		listenerPort = Integer.parseInt(adminConfig.getAttribute("port"));
 
+// Load DB persisted configuration
+		channelManagerDAO.loadConfiguration(this);
+
+		if(log.isInfoEnabled()) {
+			log.info("NNTP server listener port = " + listenerPort);
+		}
 	}
 
-	public void start() {
+	public void start() throws Exception {
+		if(listener == null) {
+			listener = new NNTPServerListener(this, listenerPort);
+		}
 		listener.start();
 	}
 
@@ -68,7 +89,19 @@ public class NNTPServer {
 	}
 
 	void handleConnection(Socket clientConnection) {
-		simpleThreadPool.run(new ClientHandler(clientConnection));
+		simpleThreadPool.run(new ClientHandler(this, clientConnection));
 	}
+	
+	public int getContentType() {
+		return contentType;
+	}
+	
+	public void setContentType(int contentType) {
+		this.contentType = contentType;
+	}
+	
+	public void saveConfiguration() {
+		channelManagerDAO.saveConfiguration(this);
+	}	
 
 }
