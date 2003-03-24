e406b8ed34660a4091d46ea5659996f4c8c224bc
jasonbrome <jasonbrome>
2003-03-24 03:15:06 +0000

    Added license info

--- src/org/methodize/nntprss/util/RSSHelper.java
+++ src/org/methodize/nntprss/util/RSSHelper.java
@@ -1,9 +1,40 @@
 package org.methodize.nntprss.util;
 
+/* -----------------------------------------------------------
+ * nntp//rss - a bridge between the RSS world and NNTP clients
+ * Copyright (c) 2002, 2003 Jason Brome.  All Rights Reserved.
+ *
+ * email: nntprss@methodize.org
+ * mail:  Methodize Solutions
+ *        PO Box 3865
+ *        Grand Central Station
+ *        New York NY 10163
+ * 
+ * This file is part of nntp//rss
+ *
+ * nntp//rss is free software; you can redistribute it 
+ * and/or modify it under the terms of the GNU General 
+ * Public License as published by the Free Software Foundation; 
+ * either version 2 of the License, or (at your option) any 
+ * later version.
+ *
+ * This program is distributed in the hope that it will be 
+ * useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+ * PURPOSE.  See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public 
+ * License along with this program; if not, write to the 
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330, 
+ * Boston, MA  02111-1307  USA
+ * ----------------------------------------------------- */
+
 /**
  * @author Jason Brome <jason@methodize.org>
- * @version 
+ * @version $Id: RSSHelper.java,v 1.2 2003/03/24 03:15:06 jasonbrome Exp $
  */
+
 public class RSSHelper {
 
 	/**
