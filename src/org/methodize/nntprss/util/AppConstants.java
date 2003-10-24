c66908c41c2657fcf1581845664acaaa93668355
jasonbrome <jasonbrome>
2003-10-24 02:36:56 +0000

    Updated version string

--- src/org/methodize/nntprss/util/AppConstants.java
+++ src/org/methodize/nntprss/util/AppConstants.java
@@ -39,7 +39,7 @@ import javax.xml.parsers.ParserConfigurationException;
 
 /**
  * @author Jason Brome <jason@methodize.org>
- * @version $Id: AppConstants.java,v 1.10 2003/09/28 21:15:20 jasonbrome Exp $
+ * @version $Id: AppConstants.java,v 1.11 2003/10/24 02:36:56 jasonbrome Exp $
  */
 public final class AppConstants {
 
@@ -57,7 +57,7 @@ public final class AppConstants {
 
 	public static final String USERS_CONFIG = "users.properties";
 
-	public static final String VERSION = "0.4-beta-1";
+	public static final String VERSION = "0.4-beta-4";
 
 
 	public static final int OPEN_ENDED_RANGE = -1;
