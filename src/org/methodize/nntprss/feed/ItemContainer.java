86d0187a5c3205e5850d04f4ad7f64d7c461e7ea
aslom <aslom>
2004-09-05 17:00:41 +0000

    make sure that last article number NEVER decreases

--- src/org/methodize/nntprss/feed/ItemContainer.java
+++ src/org/methodize/nntprss/feed/ItemContainer.java
@@ -9,24 +9,24 @@ package org.methodize.nntprss.feed;
  *        PO Box 3865
  *        Grand Central Station
  *        New York NY 10163
- * 
+ *
  * This file is part of nntp//rss
- * 
- * nntp//rss is free software; you can redistribute it 
- * and/or modify it under the terms of the GNU General 
- * Public License as published by the Free Software Foundation; 
- * either version 2 of the License, or (at your option) any 
+ *
+ * nntp//rss is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2 of the License, or (at your option) any
  * later version.
  *
- * This program is distributed in the hope that it will be 
- * useful, but WITHOUT ANY WARRANTY; without even the implied 
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
- * PURPOSE.  See the GNU General Public License for more 
+ * This program is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more
  * details.
  *
- * You should have received a copy of the GNU General Public 
- * License along with this program; if not, write to the 
- * Free Software Foundation, Inc., 59 Temple Place, Suite 330, 
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
  * Boston, MA  02111-1307  USA
  * ----------------------------------------------------- */
 
@@ -34,7 +34,7 @@ import java.util.Date;
 
 /**
  * @author Jason Brome <jason@methodize.org>
- * @version $Id: ItemContainer.java,v 1.3 2004/03/27 02:12:48 jasonbrome Exp $
+ * @version $Id: ItemContainer.java,v 1.4 2004/09/05 17:00:41 aslom Exp $
  */
 
 public class ItemContainer {
@@ -42,15 +42,15 @@ public class ItemContainer {
     protected String name;
 
     /**
-    	 * @return
-    	 */
+         * @return
+         */
     public String getName() {
         return name;
     }
 
     /**
-    	 * @param string
-    	 */
+         * @param string
+         */
     public void setName(String string) {
         name = string;
     }
@@ -62,49 +62,51 @@ public class ItemContainer {
     protected int totalArticles = 0;
 
     /**
-    	 * Returns the firstArticleNumber.
-    	 * @return long
-    	 */
+         * Returns the firstArticleNumber.
+         * @return long
+         */
     public int getFirstArticleNumber() {
         return firstArticleNumber;
     }
 
     /**
-    	 * Returns the lastArticleNumber.
-    	 * @return long
-    	 */
+         * Returns the lastArticleNumber.
+         * @return long
+         */
     public int getLastArticleNumber() {
         return lastArticleNumber;
     }
 
     /**
-    	 * Sets the firstArticleNumber.
-    	 * @param firstArticleNumber The firstArticleNumber to set
-    	 */
+         * Sets the firstArticleNumber.
+         * @param firstArticleNumber The firstArticleNumber to set
+         */
     public void setFirstArticleNumber(int firstArticleNumber) {
         this.firstArticleNumber = firstArticleNumber;
     }
 
     /**
-    	 * Sets the lastArticleNumber.
-    	 * @param lastArticleNumber The lastArticleNumber to set
-    	 */
+     * Sets the lastArticleNumber.
+     * @param lastArticleNumber The lastArticleNumber to set
+     */
     public void setLastArticleNumber(int lastArticleNumber) {
-        this.lastArticleNumber = lastArticleNumber;
+        if(this.lastArticleNumber < lastArticleNumber) { //make sure that article number NEVER decreases
+            this.lastArticleNumber = lastArticleNumber;
+        }
     }
 
     /**
-    	 * Returns the totalArticles.
-    	 * @return int
-    	 */
+         * Returns the totalArticles.
+         * @return int
+         */
     public int getTotalArticles() {
         return totalArticles;
     }
 
     /**
-    	 * Sets the totalArticles.
-    	 * @param totalArticles The totalArticles to set
-    	 */
+         * Sets the totalArticles.
+         * @param totalArticles The totalArticles to set
+         */
     public void setTotalArticles(int totalArticles) {
         this.totalArticles = totalArticles;
     }
@@ -112,15 +114,15 @@ public class ItemContainer {
     protected Date created;
 
     /**
-    	 * @return
-    	 */
+         * @return
+         */
     public Date getCreated() {
         return created;
     }
 
     /**
-    	 * @param date
-    	 */
+         * @param date
+         */
     public void setCreated(Date date) {
         created = date;
     }
